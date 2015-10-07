c077bd7990d73de7b95ea0efa555804fc3e0f603
Erica McIntosh <superhooper_22@yahoo.com>
2015-10-07 12:50:56 -0500

    added projects page and more styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-$('ul.nav.nav-pills li').click(function() {			
-    $(this).parent().addClass('active').siblings().removeClass('active');			
-});
+var hoverIn = function(){
+	$(this).next().children('div.project-desc').addClass('show-project');
+	$(this).next().children('div.project-desc').removeClass('hide-project');};
+var hoverOut = function(){
+	$(this).next().children('div.project-desc').addClass('hide-project');
+	$(this).next().children('div.project-desc').removeClass('show-project');};
+
+$('.projectpic').hover(hoverIn, hoverOut);
+
+
+
+
+
+
+
+
+
+
+
+// $('#rcrc').hover(function(){
+// 	$('.project-desc').addClass('show-project');
+// 	$('.project-desc').removeClass('hide-project');
+// }, function(){
+// 	$('.project-desc').removeClass('show-project');
+// 	$('.project-desc').addClass('hide-project');
+// });
+
